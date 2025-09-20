6c4ec08dc2fb79e5a96cb2347b20613dcd232165
OutBreak778 <mishran.2003@gmail.com>
2025-09-21 00:54:20 +0530

    added the infinite scroll and about page 2.0

--- components/About.tsx
+++ components/About.tsx
@@ -35,18 +35,23 @@ const About = () => {
       },
       width: "98%",
       borderBottomRightRadius: "65px",
-      borderBottomLeftRadius: "65px"
+      borderBottomLeftRadius: "65px",
     });
   }, []);
 
   return (
-    <div ref={slideRef} className="slideRef max-w-full mx-auto transition-all duration-400 h-full bg-black text-gray-50 md:my-2 pb-24 md:pb-0">
+    <div
+      ref={slideRef}
+      className="slideRef max-w-full mx-auto transition-all duration-400 h-full bg-black text-gray-50 md:my-2 pb-24 md:pb-0"
+    >
       <motion.div
         className="mask text-4xl md:text-5xl w-full md:px-2 px-6 h-full flex items-center justify-center cursor-context-menu"
-        animate={{
-          WebkitMaskPosition: `${x! - size / 2}px ${y! - size / 2}px`,
-          WebkitMaskSize: `${size}px`,
-        }}
+        animate={
+          {
+            WebkitMaskPosition: `${x! - size / 2}px ${y! - size / 2}px`,
+            WebkitMaskSize: `${size}px`,
+          } as any
+        } // <-- cast to any
         transition={{ type: "tween", ease: "backOut", duration: 0.7 }}
       >
         <p
@@ -69,8 +74,11 @@ const About = () => {
         <p className="w-[1000px] font-medium mt-24 md:mt-0">
           I’m <span className="text-[#F59E0B] font-bold">NIKHIL MISHRA</span>, a
           Master’s student in Computer Applications driven by a passion for
-          technology and problem-solving. I thrive on <span className="text-[#F59E0B] font-bold">learning new concepts</span>,
-          experimenting with ideas, and turning challenges into impactful
+          technology and problem-solving. I thrive on{" "}
+          <span className="text-[#F59E0B] font-bold">
+            learning new concepts
+          </span>
+          , experimenting with ideas, and turning challenges into impactful
           solutions. I’m constantly expanding my skills while enjoying the
           process of growth and discovery.
         </p>
